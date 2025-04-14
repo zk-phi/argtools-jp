9da13a293b485a0daf5af5326243074dd30a09e7
zk-phi <zk-phi@users.noreply.github.com>
2025-04-14 18:46:40 +0900

    Install plugin-dsv

---
+++ src/global.d.ts
@@ -0,0 +1,4 @@
+declare module "*.csv" {
+  const _default: Array<{ [key: string]: string }>
+  export default _default;
+}
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import dsv from "@rollup/plugin-dsv";
 import { resolve } from "path";
 
 export default defineConfig({
   root: "src/entrypoints",
   base: "/argtools-jp/",
-  plugins: [preact()],
+  plugins: [preact(), dsv()],
   publicDir: resolve(__dirname, "public"),
   build: {
     outDir: resolve(__dirname, "dist"),
