ceb9f57295028b7cb0df4f86efe562926ea8803f
zk-phi <zk-phi@users.noreply.github.com>
2025-04-17 03:07:19 +0900

    update

---
+++ src/entrypoints/drwatson/analyzers/extractors/urlExtractorFactory.tsx
@@ -0,0 +1,54 @@
+import type { ComponentChildren } from "preact";
+import { textData, type Data } from "../../datatypes";
+import type { AnalyzerModule } from "../../main";
+
+export const urlExtractorFactory = ({
+  label,
+  hint,
+  description,
+  detector,
+  extractor,
+  trimmer,
+  urlConstructor
+} : {
+  label: string,
+  hint: string,
+  description: ComponentChildren,
+  detector: RegExp,
+  extractor: RegExp,
+  trimmer: RegExp,
+  urlConstructor: (str: string) => string,
+}): AnalyzerModule => {
+  const detect = (data: Data) => (
+    data.type === "text" && data.value.match(detector) ? hint : null
+  );
+
+  const instantiate = (_: any, src: Data) => {
+    if (src.type !== "text") {
+      return { result: textData("UNEXPECTED: data is not a text.") };
+    }
+    const matches = src.value.match(extractor);
+    if (!matches) {
+      return { result: textData("UNEXPECTED: no matches.") };
+    }
+    const trimmed = matches.map(match => match.match(trimmer)![0]);
+    const urls = trimmed.map(urlConstructor);
+
+    const component = () => (
+      <>
+        {description}
+        <ul>
+          {urls.map(url => (
+            <li key={url}>
+              <a href={url} target="_blank" rel="noreferrer">{url}</a>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+
+    return { component };
+  };
+
+  return { label, detect, instantiate };
+}
--- src/entrypoints/drwatson/analyzers/extractors/youtubeAnalyzer.tsx
+++ src/entrypoints/drwatson/analyzers/extractors/youtubeAnalyzer.tsx
@@ -1,45 +1,16 @@
-import type { Data } from "../../datatypes";
-import type { AnalyzerModule } from "../../main";
+import { urlExtractorFactory } from "./urlExtractorFactory";
 
-const youtubeId = "[A-z0-9_-]{10,12}";
-const delimitedYoutubeId = `([^A-z0-9_-]|^)${youtubeId}([^A-z0-9_-]|$)`;
-const youtubeIdTester = new RegExp(delimitedYoutubeId, "m");
-const youtubeIdMatcher = new RegExp(delimitedYoutubeId, "mg");
-const youtubeIdTrimmer = new RegExp(youtubeId);
+const body = "[A-z0-9_-]{10,12}";
+const delimited = `([^A-z0-9_-]|^)${body}([^A-z0-9_-]|$)`;
 
-const detect = (data: Data) => {
-  if (data.type === "text" && data.value.match(youtubeIdTester)) {
-    return "11 文字前後の英数字または -, _";
-  }
-  return null;
-};
-
-const instantiate = (_id: number, src: Data) => {
-  const matches = src.type === "text" ? src.value.match(youtubeIdMatcher) : null;
-  const ids = matches?.map(match => {
-    const trimmed = match.match(youtubeIdTrimmer)!;
-    return trimmed[0];
-  });
-  const urls = ids?.map(id => `https://youtube.com/watch?v=${id}`);
-
-  const component = () => (
-    <>
-      <p>検出された ID 候補の一覧です（実際に動画が存在するとは限りません）</p>
-      <ul>
-        {urls?.map(url => (
-          <li key={url}>
-            <a href={url} target="_blank" rel="noreferrer">{url}</a>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-
-  return { component };
-};
-
-export const youtubeAnalyzer: AnalyzerModule = {
+export const youtubeAnalyzer = urlExtractorFactory({
   label: "YouTube の動画 ID っぽい文字列",
-  detect,
-  instantiate,
-};
+  hint: "11 文字前後の英数字または -, _",
+  description: (
+    <p>検出された ID 候補の一覧です（実際に動画が存在するとは限りません）</p>
+  ),
+  detector: new RegExp(delimited, "m"),
+  extractor: new RegExp(delimited, "mg"),
+  trimmer: new RegExp(body),
+  urlConstructor: (id: string) => `https://youtube.com/watch?v=${id}`
+});
