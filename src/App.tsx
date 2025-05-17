998baf3250c4439c11ac47ef4d194c7a62499419
zk-phi <zk-phi@users.noreply.github.com>
2025-05-18 03:56:22 +0900

    Fix: Avoid using small tag

--- src/App.tsx
+++ src/App.tsx
@@ -125,8 +125,8 @@ const AnalyzersList = () => (
       ))}
       <div>
         <b>その他</b>
-        <div style={{ marginLeft: "16px" }}>
-          <small>ファイル形式の自動判別、条件に一致する単語や地名の特定</small>
+        <div style={{ marginLeft: "16px", fontSize: "smaller" }}>
+          ファイル形式の自動判別、条件に一致する単語や地名の特定
         </div>
       </div>
     </details>
