b690c51b58d3d5f7f60f035d1df4d836d009fef0
zk-phi <zk-phi@users.noreply.github.com>
2025-06-08 17:33:21 +0900

    Fix linter issues

--- src/modules/tools/index.ts
+++ src/modules/tools/index.ts
@@ -3,7 +3,6 @@ import { reverseSpectrogram } from "./image/reverseSpectrogram";
 import { centerCanceller } from "./audio/centerCanceller";
 import { fileTypeDetector } from "./binary/fileTypeDetector";
 import { anything } from "./wordlist/anything";
-import type { AnalyzerModule } from "../";
 import type { AnalyzerCategory } from "../analyzers";
 
 export const toolCategories: AnalyzerCategory[] = [{
