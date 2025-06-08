c32220c42d6eb6429849f269edc1f9fedc87d292
zk-phi <zk-phi@users.noreply.github.com>
2025-06-08 20:41:58 +0900

    Description updates

--- pages/apps/language-detector/+config.ts
+++ pages/apps/language-detector/+config.ts
@@ -1,5 +1,5 @@
 export default {
   backlink: true,
   title: "言語判定ツール",
-  description: "プログラミング言語や自然言語を AI パワーで推定します。",
+  description: "言語がわからないプログラムや文章を AI パワーで推定します。",
 }
--- pages/index/+config.ts
+++ pages/index/+config.ts
@@ -1,5 +1,5 @@
 export default {
   backlink: false,
   title: "ARGTOOLS - 謎解き・データ解析の万能アシスタント",
-  description: "データ解析・暗号解読ツールの集合体です。データを入力すると、自動でツールを選定します。",
+  description: "データ解析・暗号解読ツールの集合体です。データを入力すると、自動で使えるツールを推薦します。",
 };
