9adab975e5f299286dd2747307d16ec8d402b439
zk-phi <zk-phi@users.noreply.github.com>
2025-06-08 11:43:03 +0000

    deploy: ae78075fe468d39cf311f90e76e737c9ded06ba6

---
+++ assets/chunks/chunk-luHbQekC.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-DKC_3VCJ.js";import{c as r}from"./chunk-CHBlyN7Y.js";const t={...r,label:"プログラミング言語・自然言語の自動推定",app:"/argtools-jp/apps/language-detector",description:a("p",{children:"長いテキストほど検出精度が上がります。"})};export{t as l};
