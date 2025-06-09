033d032880ed79a7950499b2dcf9055527031316
zk-phi <zk-phi@users.noreply.github.com>
2025-06-09 15:15:13 +0900

    UI: Defer should wait just 1 frame, instead of 100ms

--- src/utils/ui/defer.ts
+++ src/utils/ui/defer.ts
@@ -1,2 +1,4 @@
 // Give react a chance to update the UI.
-export const defer = (): Promise<void> => new Promise(resolve => setTimeout(resolve, 100));
+export const defer = (): Promise<void> => new Promise(resolve => {
+  requestAnimationFrame(() => resolve());
+});
