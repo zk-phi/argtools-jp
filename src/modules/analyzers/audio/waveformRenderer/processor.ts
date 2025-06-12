403f9cf92d99778280e7ee4ea97240db839c8af5
zk-phi <zk-phi@users.noreply.github.com>
2025-06-12 23:33:44 +0900

    Fix waveform size

--- src/modules/analyzers/audio/waveformRenderer/processor.ts
+++ src/modules/analyzers/audio/waveformRenderer/processor.ts
@@ -41,13 +41,12 @@ const renderAnalyzedData = (
   color: string,
 ): Promise<Blob> => new Promise((resolve, reject) => {
   const canvas = document.createElement("canvas");
-  canvas.width = analyzedData.length;
+  canvas.width = analyzedData.length / 2;
   canvas.height = h;
 
   const ctx = canvas.getContext("2d")!;
   ctx.strokeStyle = color;
-  const numLines = analyzedData.length / 2;
-  for (let i = 0; i < numLines; i++) {
+  for (let i = 0; i < canvas.width; i++) {
     ctx.beginPath();
     ctx.moveTo(i, (1 - analyzedData[i * 2]) / 2 * h); // min
     ctx.lineTo(i, (1 - analyzedData[i * 2 + 1]) / 2 * h); // max
@@ -79,7 +78,7 @@ export const processor = async (input: Data, reporter: StateReporter) => {
   const datum: AtomicData[] = await Promise.all(
     mapRange(buffer.numberOfChannels, async ch => {
       const channelData = buffer.getChannelData(ch);
-      const waveform = await renderWaveform(channelData, 800, 200, "#56c7ff");
+      const waveform = await renderWaveform(channelData, 640, 240, "#56c7ff");
       return await binaryData(waveform, `Ch ${ch + 1} の波形`);
     })
   );
