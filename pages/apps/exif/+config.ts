c61dbda7a9160da342bddc00391a9b93961f9987
zk-phi <zk-phi@users.noreply.github.com>
2025-06-13 02:22:26 +0900

    Tool description

--- pages/apps/exif/+config.ts
+++ pages/apps/exif/+config.ts
@@ -1,5 +1,5 @@
 export default {
   backlink: true,
-  title: "画像から Exif 情報などを抽出するツール",
-  description: "写真の撮影地・日時・機材などを特定できることがあります。Exif 以外の情報にも一部対応します。",
+  title: "画像から Exif, IPTC などを抽出するツール",
+  description: "写真の撮影地・日時・機材などを特定したり、撮影者によるコメントなどを確認できることがあります。",
 }
