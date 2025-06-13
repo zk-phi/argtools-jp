c44c782ad4ff62867bae657862b14eedea9fe70f
zk-phi <zk-phi@users.noreply.github.com>
2025-06-13 18:17:45 +0900

    Add tsuitachi, youka to suggestor

--- src/modules/analyzers/suggestions/hifumiSuggestor.tsx
+++ src/modules/analyzers/suggestions/hifumiSuggestor.tsx
@@ -1,10 +1,10 @@
 import type { Data } from "../../../datatypes";
 import type { AnalyzerModule } from "../../";
 
-const hifumi = /^(\s*[ひふみよいむなやこと])+\s*$/;
+const hifumi = /^(\s*[つひふみよいむなやよこと])+\s*$/;
 const detect = (data: Data) => {
   if (data.type === "text" && data.value.match(hifumi)) {
-    return "「ひふみよいむなやこと」の並び → ひふみ祝詞かも？";
+    return "「つひふみよいむなやよこと」の並び → ひふみ祝詞かも？";
   }
   return null;
 };
@@ -15,16 +15,16 @@ const component = () => (
     <div style={{ display: "inline-block" }}>
       <table>
         <tbody>
-          <tr><td>１</td><td>ひ（とつ）</td></tr>
-          <tr><td>２</td><td>ふ（たつ）</td></tr>
-          <tr><td>３</td><td>み（っつ）</td></tr>
-          <tr><td>４</td><td>よ（っつ）</td></tr>
-          <tr><td>５</td><td>い（つつ）</td></tr>
-          <tr><td>６</td><td>む（っつ）</td></tr>
-          <tr><td>７</td><td>な（なつ）</td></tr>
-          <tr><td>８</td><td>や（っつ）</td></tr>
-          <tr><td>９</td><td>こ（このつ）</td></tr>
-          <tr><td>１０</td><td>と（お）</td></tr>
+          <tr><td>１</td><td>ひ（とつ）・つ（いたち）</td></tr>
+          <tr><td>２</td><td>ふ（たつ）・ふ（つか）</td></tr>
+          <tr><td>３</td><td>み（っつ）・み（っか）</td></tr>
+          <tr><td>４</td><td>よ（っつ）・よ（っか）</td></tr>
+          <tr><td>５</td><td>い（つつ）・い（つか）</td></tr>
+          <tr><td>６</td><td>む（っつ）・む（いか）</td></tr>
+          <tr><td>７</td><td>な（なつ）・な（のか）</td></tr>
+          <tr><td>８</td><td>や（っつ）・よ（うか）</td></tr>
+          <tr><td>９</td><td>こ（このつ）・こ（このか）</td></tr>
+          <tr><td>１０</td><td>と（お）・と（おか）</td></tr>
         </tbody>
       </table>
     </div>
