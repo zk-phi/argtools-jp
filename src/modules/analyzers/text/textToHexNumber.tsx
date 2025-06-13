acb4994a5120a0727a34c4c9c80b38ada379f212
zk-phi <zk-phi@users.noreply.github.com>
2025-06-13 23:50:28 +0900

    Accept 6-bytes long integer
    because js support 51bits (= 6.3 bytes) -long integer

--- src/modules/analyzers/text/textToHexNumber.tsx
+++ src/modules/analyzers/text/textToHexNumber.tsx
@@ -2,14 +2,14 @@ import { simpleTextDecoderFactory } from "../../analyzerFactories";
 import { numberData } from "../../../datatypes";
 
 const alphabet = "[0-9a-fA-F]";
-// require 1-4 bytes, (if "0x" is not prefixed, 2-4)
+// require 1-6 bytes, (if "0x" is not prefixed, 2-6)
 // note that integers over 5 bytes are "bigint" in JS, which cannot be mixed with "number"s
-const body = `(0x${alphabet}{1,4}|${alphabet}{2,4})`;
+const body = `(0x${alphabet}{1,6}|${alphabet}{2,6})`;
 const delimited = `(?<=[^0-9a-fA-F]|^)${body}(?=[^0-9a-fA-F]|$)`;
 
 export const textToHexNumber = simpleTextDecoderFactory({
   label: "数値（十六進数）を抽出",
-  hint: "0-9, A-F の英数字列（２〜４桁程度）→ なんらかの数値（十六進数）かも？",
+  hint: "0-9, A-F の英数字列（２〜６桁程度）→ なんらかの数値（十六進数）かも？",
   pattern: delimited,
   decoder: (str: string, label: string) => (
     numberData(Number.parseInt(str, 16), label)
