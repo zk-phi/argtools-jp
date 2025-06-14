7ec1ea714f65b849f1bdfaa91206831f2127cd48
zk-phi <zk-phi@users.noreply.github.com>
2025-06-14 07:16:09 +0000

    deploy: 699ea410132617b924e9c7f92293b62aeeaf98d2

--- assets/chunks/chunk-DbjJWohn.js
+++
@@ -1,3 +0,0 @@
-import{e as c}from"./chunk-BBGo2Y6T.js";import"./chunk-16BH05ft.js";const l=/(\r\n?|\n)/,m=async(r,o,n)=>{if(r.type!=="text")throw new Error("テキストデータではありません");await o({status:"並べ替えています"});const t=r.value.match(l);if(!t)return r;const a=r.value.replace(t[0],`
-`).split(`
-`).sort((e,s)=>e===s?0:e<s?n==="asc"?-1:1:n==="asc"?1:-1).join(t[0]);return c(a,r.label,"並べ替えたテキスト")};export{m as processor};
