699ea410132617b924e9c7f92293b62aeeaf98d2
zk-phi <zk-phi@users.noreply.github.com>
2025-06-14 16:14:59 +0900

    Fix newline handling

--- src/modules/analyzers/text/sortLines/processor.ts
+++ src/modules/analyzers/text/sortLines/processor.ts
@@ -2,19 +2,23 @@
 import type { StateReporter } from "../../../";
 import { textData, type Data, } from "../../../../datatypes";
 
-const anyNewline = /(\r\n?|\n)/
+const anyNewline = /(\r\n?|\n)/;
+const allNewlines = /(\r\n?|\n)/g;
 export const processor = async (input: Data, reporter: StateReporter, mode: string) => {
   if (input.type !== "text") {
     throw new Error("テキストデータではありません");
   }
 
   await reporter({ status: "並べ替えています" });
+
+  // Determine newline type (LF, CR, CRLF)
+  // We don't support mixed newlines for now
   const newline = input.value.match(anyNewline);
   if (!newline) {
     return input;
   }
-  // We don't care mixed (CRLF and CR) newlines for now
-  const lnString = input.value.replace(newline[0], "\n");
+
+  const lnString = input.value.replaceAll(allNewlines, "\n");
 
   const lines = lnString.split("\n");
   const sorted = lines.sort((a, b) => {
