0d091cb4785671d78287fc5ec7d8e807c7556c3f
zk-phi <zk-phi@users.noreply.github.com>
2025-06-24 14:26:49 +0900

    Add 2LSB images to stegano analyzer

--- src/modules/analyzers/image/steganoAnalyzer/processor.ts
+++ src/modules/analyzers/image/steganoAnalyzer/processor.ts
@@ -22,7 +22,7 @@ export const processor = async (input: Data, reporter: StateReporter) => {
   const url = toBlobUrl(input);
   const image = await urlToImg(url);
 
-  await reporter({ status: "画像を描画しています 1/9" });
+  await reporter({ status: "画像を描画しています 1/14" });
   const rImg = await applyFilter(image, (arr) => {
     for (let i = 0; i < arr.length; i += 4) {
       arr[i + 1] = arr[i + 0];
@@ -31,7 +31,7 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     }
   });
 
-  await reporter({ status: "画像を描画しています 2/9" });
+  await reporter({ status: "画像を描画しています 2/14" });
   const gImg = await applyFilter(image, (arr) => {
     for (let i = 0; i < arr.length; i += 4) {
       arr[i + 0] = arr[i + 1];
@@ -40,7 +40,7 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     }
   });
 
-  await reporter({ status: "画像を描画しています 3/9" });
+  await reporter({ status: "画像を描画しています 3/14" });
   const bImg = await applyFilter(image, (arr) => {
     for (let i = 0; i < arr.length; i += 4) {
       arr[i + 0] = arr[i + 2];
@@ -49,7 +49,7 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     }
   });
 
-  await reporter({ status: "画像を描画しています 4/9" });
+  await reporter({ status: "画像を描画しています 4/14" });
   const aImg = await applyFilter(image, (arr) => {
     for (let i = 0; i < arr.length; i += 4) {
       arr[i + 0] = arr[i + 3];
@@ -58,14 +58,14 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     }
   });
 
-  await reporter({ status: "画像を描画しています 5/9" });
+  await reporter({ status: "画像を描画しています 5/14" });
   const lsbImg = await applyFilter(image, (arr) => {
     for (let i = 0; i < arr.length; i += 1) {
       arr[i] = (arr[i] & 1) * 255;
     }
   });
 
-  await reporter({ status: "画像を描画しています 6/9" });
+  await reporter({ status: "画像を描画しています 6/14" });
   const rLsbImg = await applyFilter(image, (arr) => {
     for (let i = 0; i < arr.length; i += 4) {
       arr[i + 0] = (arr[i + 0] & 1) * 255;
@@ -74,7 +74,7 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     }
   });
 
-  await reporter({ status: "画像を描画しています 7/9" });
+  await reporter({ status: "画像を描画しています 7/14" });
   const gLsbImg = await applyFilter(image, (arr) => {
     for (let i = 0; i < arr.length; i += 4) {
       arr[i + 0] = (arr[i + 1] & 1) * 255;
@@ -83,7 +83,7 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     }
   });
 
-  await reporter({ status: "画像を描画しています 8/9" });
+  await reporter({ status: "画像を描画しています 8/14" });
   const bLsbImg = await applyFilter(image, (arr) => {
     for (let i = 0; i < arr.length; i += 4) {
       arr[i + 0] = (arr[i + 2] & 1) * 255;
@@ -92,7 +92,7 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     }
   });
 
-  await reporter({ status: "画像を描画しています 9/9" });
+  await reporter({ status: "画像を描画しています 9/14" });
   const aLsbImg = await applyFilter(image, (arr) => {
     for (let i = 0; i < arr.length; i += 4) {
       arr[i + 0] = (arr[i + 3] & 1) * 255;
@@ -101,75 +101,162 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     }
   });
 
+  await reporter({ status: "画像を描画しています 10/14" });
+  const lsb2Img = await applyFilter(image, (arr) => {
+    for (let i = 0; i < arr.length; i += 1) {
+      arr[i] = (arr[i] & 2) * 85;
+    }
+  });
+
+  await reporter({ status: "画像を描画しています 11/14" });
+  const rLsb2Img = await applyFilter(image, (arr) => {
+    for (let i = 0; i < arr.length; i += 4) {
+      arr[i + 0] = (arr[i + 0] & 2) * 85;
+      arr[i + 1] = (arr[i + 0] & 2) * 85;
+      arr[i + 2] = (arr[i + 0] & 2) * 85;
+    }
+  });
+
+  await reporter({ status: "画像を描画しています 12/14" });
+  const gLsb2Img = await applyFilter(image, (arr) => {
+    for (let i = 0; i < arr.length; i += 4) {
+      arr[i + 0] = (arr[i + 1] & 2) * 85;
+      arr[i + 1] = (arr[i + 1] & 2) * 85;
+      arr[i + 2] = (arr[i + 1] & 2) * 85;
+    }
+  });
+
+  await reporter({ status: "画像を描画しています 13/14" });
+  const bLsb2Img = await applyFilter(image, (arr) => {
+    for (let i = 0; i < arr.length; i += 4) {
+      arr[i + 0] = (arr[i + 2] & 2) * 85;
+      arr[i + 1] = (arr[i + 2] & 2) * 85;
+      arr[i + 2] = (arr[i + 2] & 2) * 85;
+    }
+  });
+
+  await reporter({ status: "画像を描画しています 14/14" });
+  const aLsb2Img = await applyFilter(image, (arr) => {
+    for (let i = 0; i < arr.length; i += 4) {
+      arr[i + 0] = (arr[i + 3] & 2) * 85;
+      arr[i + 1] = (arr[i + 3] & 2) * 85;
+      arr[i + 2] = (arr[i + 3] & 2) * 85;
+    }
+  });
+
   const [canvas, ctx] = imgToCanvas(image);
   const { data: arr } = ctx.getImageData(0, 0, canvas.width, canvas.height)
 
-  await reporter({ status: "バイナリを抽出しています 1/8" });
+  await reporter({ status: "バイナリを抽出しています 1/12" });
   const rBinary = new Uint8Array(Math.ceil(arr.length / 4));
   for (let i = 0; i < rBinary.length; i++) {
     rBinary[i] = arr[i * 4 + 0];
   }
 
-  await reporter({ status: "バイナリを抽出しています 2/8" });
+  await reporter({ status: "バイナリを抽出しています 2/12" });
   const gBinary = new Uint8Array(Math.ceil(arr.length / 4));
   for (let i = 0; i < gBinary.length; i++) {
     gBinary[i] = arr[i * 4 + 1];
   }
 
-  await reporter({ status: "バイナリを抽出しています 3/8" });
+  await reporter({ status: "バイナリを抽出しています 3/12" });
   const bBinary = new Uint8Array(Math.ceil(arr.length / 4));
   for (let i = 0; i < bBinary.length; i++) {
     bBinary[i] = arr[i * 4 + 2];
   }
 
-  await reporter({ status: "バイナリを抽出しています 4/8" });
+  await reporter({ status: "バイナリを抽出しています 4/12" });
   const aBinary = new Uint8Array(Math.ceil(arr.length / 4));
   for (let i = 0; i < aBinary.length; i++) {
     aBinary[i] = arr[i * 4 + 3];
   }
 
-  await reporter({ status: "バイナリを抽出しています 5/8" });
+  await reporter({ status: "バイナリを抽出しています 5/12" });
   const rLsbBinary = new Uint8Array(Math.ceil(arr.length / 8));
   for (let i = 0; i < rLsbBinary.length; i++) {
     let value = 0;
     for (let j = 0; j < 8; j++) {
-      const ix = (i * 8 + j) * 4;
-      value = value * 2 + (ix + 0 < arr.length ? arr[ix + 0] & 1 : 0);
+      const ix = (i * 8 + j) * 4 + 0;
+      value = value * 2 + (ix < arr.length ? arr[ix] & 1 : 0);
     }
     rLsbBinary[i] = value;
   }
 
-  await reporter({ status: "バイナリを抽出しています 6/8" });
+  await reporter({ status: "バイナリを抽出しています 6/12" });
   const gLsbBinary = new Uint8Array(Math.ceil(arr.length / 8));
   for (let i = 0; i < gLsbBinary.length; i++) {
     let value = 0;
     for (let j = 0; j < 8; j++) {
-      const ix = (i * 8 + j) * 4;
-      value = value * 2 + (ix + 1 < arr.length ? arr[ix + 1] & 1 : 0);
+      const ix = (i * 8 + j) * 4 + 1;
+      value = value * 2 + (ix < arr.length ? arr[ix] & 1 : 0);
     }
     gLsbBinary[i] = value;
   }
 
-  await reporter({ status: "バイナリを抽出しています 7/8" });
+  await reporter({ status: "バイナリを抽出しています 7/12" });
   const bLsbBinary = new Uint8Array(Math.ceil(arr.length / 8));
   for (let i = 0; i < bLsbBinary.length; i++) {
     let value = 0;
     for (let j = 0; j < 8; j++) {
-      const ix = (i * 8 + j) * 4;
-      value = value * 2 + (ix + 2 < arr.length ? arr[ix + 2] & 1 : 0);
+      const ix = (i * 8 + j) * 4 + 2;
+      value = value * 2 + (ix < arr.length ? arr[ix] & 1 : 0);
     }
     bLsbBinary[i] = value;
   }
 
-  await reporter({ status: "バイナリを抽出しています 8/8" });
+  await reporter({ status: "バイナリを抽出しています 8/12" });
   const aLsbBinary = new Uint8Array(Math.ceil(arr.length / 8));
   for (let i = 0; i < aLsbBinary.length; i++) {
     let value = 0;
     for (let j = 0; j < 8; j++) {
-      const ix = (i * 8 + j) * 4;
-      value = value * 2 + (ix + 3 < arr.length ? arr[ix + 3] & 1 : 0);
+      const ix = (i * 8 + j) * 4 + 3;
+      value = value * 2 + (ix < arr.length ? arr[ix] & 1 : 0);
     }
     aLsbBinary[i] = value;
+  }
+
+  await reporter({ status: "バイナリを抽出しています 9/12" });
+  const rLsb2Binary = new Uint8Array(Math.ceil(arr.length / 4));
+  for (let i = 0; i < rLsb2Binary.length; i++) {
+    let value = 0;
+    for (let j = 0; j < 4; j++) {
+      const ix = (i * 4 + j) * 4 + 0;
+      value = value * 4 + (ix < arr.length ? arr[ix] & 2 : 0);
+    }
+    rLsb2Binary[i] = value;
+  }
+
+  await reporter({ status: "バイナリを抽出しています 10/12" });
+  const gLsb2Binary = new Uint8Array(Math.ceil(arr.length / 4));
+  for (let i = 0; i < gLsb2Binary.length; i++) {
+    let value = 0;
+    for (let j = 0; j < 8; j++) {
+      const ix = (i * 4 + j) * 4 + 1;
+      value = value * 4 + (ix < arr.length ? arr[ix] & 2 : 0);
+    }
+    gLsb2Binary[i] = value;
+  }
+
+  await reporter({ status: "バイナリを抽出しています 11/12" });
+  const bLsb2Binary = new Uint8Array(Math.ceil(arr.length / 4));
+  for (let i = 0; i < bLsb2Binary.length; i++) {
+    let value = 0;
+    for (let j = 0; j < 8; j++) {
+      const ix = (i * 4 + j) * 4 + 2;
+      value = value * 4 + (ix < arr.length ? arr[ix] & 2 : 0);
+    }
+    bLsb2Binary[i] = value;
+  }
+
+  await reporter({ status: "バイナリを抽出しています 12/12" });
+  const aLsb2Binary = new Uint8Array(Math.ceil(arr.length / 4));
+  for (let i = 0; i < aLsb2Binary.length; i++) {
+    let value = 0;
+    for (let j = 0; j < 8; j++) {
+      const ix = (i * 8 + j) * 4 + 3;
+      value = value * 4 + (ix < arr.length ? arr[ix] & 2 : 0);
+    }
+    aLsb2Binary[i] = value;
   }
 
   await reporter({ status: "データを整形しています" });
@@ -183,6 +270,11 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     await binaryData(new Uint8Array(await gLsbImg.arrayBuffer()), "G の最下位ビットを抽出"),
     await binaryData(new Uint8Array(await bLsbImg.arrayBuffer()), "B の最下位ビットを抽出"),
     await binaryData(new Uint8Array(await aLsbImg.arrayBuffer()), "A の最下位ビットを抽出"),
+    await binaryData(new Uint8Array(await lsb2Img.arrayBuffer()), "下位２ビットを抽出"),
+    await binaryData(new Uint8Array(await rLsb2Img.arrayBuffer()), "R の下位２ビットを抽出"),
+    await binaryData(new Uint8Array(await gLsb2Img.arrayBuffer()), "G の下位２ビットを抽出"),
+    await binaryData(new Uint8Array(await bLsb2Img.arrayBuffer()), "B の下位２ビットを抽出"),
+    await binaryData(new Uint8Array(await aLsb2Img.arrayBuffer()), "A の下位２ビットを抽出"),
     await binaryData(rBinary, "R の画素値をデータ化"),
     await binaryData(gBinary, "G の画素値をデータ化"),
     await binaryData(bBinary, "B の画素値をデータ化"),
@@ -191,5 +283,9 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     await binaryData(gLsbBinary, "G の最下位ビットをデータ化"),
     await binaryData(bLsbBinary, "B の最下位ビットをデータ化"),
     await binaryData(aLsbBinary, "A の最下位ビットをデータ化"),
+    await binaryData(rLsb2Binary, "R の下位２ビットをデータ化"),
+    await binaryData(gLsb2Binary, "G の下位２ビットをデータ化"),
+    await binaryData(bLsb2Binary, "B の下位２ビットをデータ化"),
+    await binaryData(aLsb2Binary, "A の下位２ビットをデータ化"),
   ]);
 };
