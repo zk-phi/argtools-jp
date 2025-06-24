625d43f08364c947eedb70afc179a96d17246dfe
zk-phi <zk-phi@users.noreply.github.com>
2025-06-24 10:52:43 +0900

    Add some more image merging methods

--- src/modules/analyzers/image/imageMerger/processor.ts
+++ src/modules/analyzers/image/imageMerger/processor.ts
@@ -36,6 +36,33 @@ const _makeImage = async (
   })
 );
 
+// https://qiita.com/kerupani129/items/4bf75d9f44a5b926df58
+//    A  = Ab * (1 - Af) + Af
+//    C' = Ab * F(Cb, Cf) + (1 - Ab) * Cf
+//    C  = (Ab * (1 - Af) * Cb + Af * C') / A
+const compositeImage = (
+  w: number,
+  h: number,
+  b: Uint8ClampedArray,
+  f: Uint8ClampedArray,
+  blendFn: (b: number, f: number) => number,
+): Promise<Blob> => _makeImage(w, h, arr => {
+  for (let i = 0; i < arr.length; i += 4) {
+    const ab = b[i + 3] / 255;
+    const af = f[i + 3] / 255;
+    const alpha = ab * (1 - af) + af;
+    const c = [
+      ab * blendFn(b[i + 0], f[i + 0]) + (1 - ab) * f[i + 0],
+      ab * blendFn(b[i + 1], f[i + 1]) + (1 - ab) * f[i + 1],
+      ab * blendFn(b[i + 2], f[i + 2]) + (1 - ab) * f[i + 2],
+    ];
+    arr[i + 0] = Math.round((ab * (1 - af) * b[i + 0] + af * c[0]) / alpha);
+    arr[i + 1] = Math.round((ab * (1 - af) * b[i + 1] + af * c[1]) / alpha);
+    arr[i + 2] = Math.round((ab * (1 - af) * b[i + 2] + af * c[2]) / alpha);
+    arr[i + 3] = Math.round(alpha * 255);
+  }
+});
+
 const MAX_DISTANCE = Math.sqrt(255 ** 2 * 3);
 
 export const processor = async (input: Data, reporter: StateReporter) => {
@@ -63,29 +90,19 @@ export const processor = async (input: Data, reporter: StateReporter) => {
   const arr1 = data1.data;
   const arr2 = data2.data;
 
-  // https://qiita.com/kerupani129/items/4bf75d9f44a5b926df58
-  // given B(Cb, Cf) = Cf,
-  //     A = Ab(1 - Af) + Af
-  //   Cbf = AbB(Cb,Cf) + (1 - Ab)Cf
-  //       = AbCf + (1 - Ab)Cf
-  //       = AbCf + Cf - AbCf
-  //       = Cf
-  //     C = (Ab(1 - Af)Cb + AfCbf) / A
-  //       = (Ab(1 - Af)Cb + AfCf) / A
-  await reporter({ status: "描画しています 1/3" });
-  const mergeImage = await _makeImage(w, h, arr => {
-    for (let i = 0; i < arr.length; i += 4) {
-      const a1 = arr1[i + 3] / 255;
-      const a2 = arr2[i + 3] / 255;
-      const alpha = a1 * (1 - a2) + a2;
-      arr[i + 0] = Math.round((a1 * (1 - a2) * arr1[i + 0] + a2 * arr2[i + 0]) / alpha);
-      arr[i + 1] = Math.round((a1 * (1 - a2) * arr1[i + 1] + a2 * arr2[i + 1]) / alpha);
-      arr[i + 2] = Math.round((a1 * (1 - a2) * arr1[i + 2] + a2 * arr2[i + 2]) / alpha);
-      arr[i + 3] = Math.round(alpha * 255);
-    }
-  });
+  await reporter({ status: "描画しています 1/6" });
+  const mergeImage = await compositeImage(w, h, arr1, arr2, (b, f) => f);
 
-  await reporter({ status: "描画しています 2/3" });
+  await reporter({ status: "描画しています 2/6" });
+  const mergeImageR = await compositeImage(w, h, arr2, arr1, (b, f) => f);
+
+  await reporter({ status: "描画しています 3/6" });
+  const addImage = await compositeImage(w, h, arr2, arr1, (b, f) => Math.min(255, b + f));
+
+  await reporter({ status: "描画しています 4/6" });
+  const mulImage = await compositeImage(w, h, arr2, arr1, (b, f) => b * f / 255);
+
+  await reporter({ status: "描画しています 5/6" });
   const xorImage = await _makeImage(w, h, arr => {
     for (let i = 0; i < arr.length; i += 4) {
       arr[i + 0] = arr1[i + 0] ^ arr2[i + 0];
@@ -95,7 +112,7 @@ export const processor = async (input: Data, reporter: StateReporter) => {
     }
   });
 
-  await reporter({ status: "描画しています 3/3" });
+  await reporter({ status: "描画しています 6/6" });
   const distanceImage = await _makeImage(w, h, arr => {
     let max = 0;
     const values = new Float32Array(arr.length / 4);
@@ -123,8 +140,29 @@ export const processor = async (input: Data, reporter: StateReporter) => {
 
   await reporter({ status: "データを整形しています" });
   return multipleData([
-    await binaryData(new Uint8Array(await mergeImage.arrayBuffer()), "単純に重ねる"),
-    await binaryData(new Uint8Array(await xorImage.arrayBuffer()), "RGB 値の XOR"),
-    await binaryData(new Uint8Array(await distanceImage.arrayBuffer()), "RGB 値の距離"),
+    await binaryData(
+      new Uint8Array(await mergeImage.arrayBuffer()),
+      "単純に重ねる",
+    ),
+    await binaryData(
+      new Uint8Array(await mergeImageR.arrayBuffer()),
+      "単純に重ねる (逆順)",
+    ),
+    await binaryData(
+      new Uint8Array(await addImage.arrayBuffer()),
+      "加算合成",
+    ),
+    await binaryData(
+      new Uint8Array(await mulImage.arrayBuffer()),
+      "乗算合成",
+    ),
+    await binaryData(
+      new Uint8Array(await xorImage.arrayBuffer()),
+      "RGB 値の XOR",
+    ),
+    await binaryData(
+      new Uint8Array(await distanceImage.arrayBuffer()),
+      "RGB 値の距離",
+    ),
   ]);
 };
