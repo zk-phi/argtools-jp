2e41d24e27f0b6dee703d16c37a96644c8b2f2cd
zk-phi <zk-phi@users.noreply.github.com>
2025-06-27 23:05:07 +0900

    Fix linter issues

--- src/modules/analyzers/text/showInvisibles/index.ts
+++ src/modules/analyzers/text/showInvisibles/index.ts
@@ -4,7 +4,6 @@ import type { Data } from "../../../../datatypes";
 
 const NON_ASCII = /[^\x20-\x7e]/;
 const detect = (data: Data) => {
-  console.log(data);
   if (data.type === "text" && data.value.length > 3 && data.value.match(NON_ASCII)) {
     return "もしかしたら、見えない文字がどこかに埋め込まれているかも？";
   }
