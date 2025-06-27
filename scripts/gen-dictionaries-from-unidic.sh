94154bc3d506b92ea722fb4360d6b54be946f890
zk-phi <zk-phi@users.noreply.github.com>
2025-06-27 19:39:07 +0900

    Use shell to format jawiki-all-titles

--- scripts/gen-dictionaries-from-unidic.sh
+++ scripts/gen-dictionaries-from-unidic.sh
@@ -4,3 +4,6 @@ cat lex.csv | grep -e ",普通名詞" -e ",代名詞" -e ",数詞" -e ",固有�
 cat lex.csv | grep -e ",動詞" | cut -f 15,5,6,7 -d "," | nkf -Z | sort | uniq > verbs
 cat lex.csv | grep -e ",形状詞" -e ",感動詞" -e ",形容詞" -e ",副詞" -e ",連体詞" -e ",接続詞" | cut -f 15,5,6,7 -d "," | nkf -Z | sort | uniq > adjectives
 cat lex.csv | grep -e ",固有名詞,人名" | cut -f 15,5,6,7 -d "," | nkf -Z | sort | uniq > propers
+
+# Format wikipedia page list
+cat jawiki-all-titles | grep "^0" | cut -f 2 | sed -E "s;_\([^\)]*\)$;;g" | sed "s;_; ;g" | sort | uniq > jawiki-all-titles-formatted
